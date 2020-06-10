747c69be5a54d856b087eba938e0db2576136e51
pierreEnoc <pierre_enoc@yahoo.fr>
2020-06-10 18:10:00 -0300

    date format

--- src/main/kotlin/com/pierre/demo/model/Cliente.kt
+++ src/main/kotlin/com/pierre/demo/model/Cliente.kt
@@ -1,12 +1,18 @@
 package com.pierre.demo.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import java.util.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class Cliente (val id: Long, val nome: String, val dataNascimento: Date, var telefone: Telefone?)
+data class Cliente (val id: Long,
+                    val nome: String,
+                    @JsonFormat(shape = JsonFormat.Shape.STRING,
+                    pattern = "dd/MM/YYYY")
+                    val dataNascimento: Date,
+                    var telefone: Telefone?)
 
 @JsonIgnoreProperties("tipo")
 data class Telefone(val ddd: String="", val numero : String, @JsonIgnore val tipo: String= "")
